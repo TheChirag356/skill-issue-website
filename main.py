af8ba5331226a039062fcdc56fe1462feda46f4a
Chirag Arora <54665586+TheChirag356@users.noreply.github.com>
2024-05-08 18:14:02 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/members")
+def members():
+    return render_template("members.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("page_not_found.html"), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
