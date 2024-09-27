41eea68c610e00f5f0e3133ae4641e1bbc2eed92
TheChirag356 <arorachirag12005@gmail.com>
2024-09-27 12:40:35 +0530

    Update package-lock.json and package.json to add daisyui dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
@@ -12,5 +13,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
 };
